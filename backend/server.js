17bd3a5e23a8cc673613c5bb57272f9bb55c7dbc
Code Subtle <korvanvitthal@gmail.com>
2025-10-31 19:13:55 +0530

    server socket created!

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const app = require("./src/app")
 const connectDB = require('./src/db/db')
+const initSocketServer = require("./src/sockets/socket.server");
+const httpServer = require("http").createServer(app);
+
 
 
 connectDB()
+initSocketServer(httpServer);
 
-app.listen(3000, ()=>{
-      console.log("Server is running on port 3000");
-      
-})
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
