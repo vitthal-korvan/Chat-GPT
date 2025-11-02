6d91bc13199a8808057be8e90cede4a72739a469
Code Subtle <korvanvitthal@gmail.com>
2025-11-02 11:12:18 +0530

    socket sever connection created with user token

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,10 +1,41 @@
 const { Server } = require("socket.io")
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+// const aiService = require("../services/ai.service");
+// const messageModel = require("../models/message.model");
+// const { createMemory, queryMemory } = require("../services/vector.service");
 
 function initSocketServer(httpServer) {
 
     const io = new Server(httpServer, {})
 
+    io.use(async (socket, next) => {
+      const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+      // console.log("Socket Connection Created!!!!");
+      
+
+      if (!cookies.token) {
+        next(new Error("Authentication error: No token provided"));
+      }
+
+      try {
+        const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+        const user = await userModel.findById(decoded.id);
+
+        socket.user = user;
+
+        next();
+      } catch (err) {
+        next(new Error("Authentication error: Invalid token"));
+      }
+    });   
+
     io.on('connection', (socket)=>{
+      console.table(socket.user);
+      
       console.log("New socket connection", socket.id);
       
     })
